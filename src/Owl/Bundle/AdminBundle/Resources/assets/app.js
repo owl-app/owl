3ff04798bbd8d54e1821cc0cc1d5eeffe1280949
pawelkeska <projekty@pawelkeska.eu>
2025-03-11 23:50:37 +0100

    [Ui] Admin

--- src/Owl/Bundle/AdminBundle/Resources/assets/app.js
+++ src/Owl/Bundle/AdminBundle/Resources/assets/app.js
@@ -7,53 +7,68 @@ import './sylius-require-confirmation';
 import './sylius-toggle';
 import './sylius-check-all';
 
+import { startStimulusApp } from '@symfony/stimulus-bridge';
+import LiveController from '@symfony/ux-live-component';
+import '@symfony/ux-live-component/styles/live.css';
+
+// Registers Stimulus controllers from controllers.json and in the controllers/ directory
+export const app = startStimulusApp(require.context(
+  '@symfony/stimulus-bridge/lazy-controller-loader!./controllers',
+  true,
+  /\.[jt]sx?$/
+));
+
+app.register('live', LiveController);
+
+app.debug = process.env.NODE_ENV !== 'production';
+
 document.addEventListener("turbo:load", () => {
-  $('#sidebar').addClass('visible');
-  $('#sidebar').sidebar('attach events', '#sidebar-toggle', 'toggle');
-  $('#sidebar').sidebar('setting', {
-    dimPage: false,
-    closable: false,
-  });
+  // $('#sidebar').addClass('visible');
+  // $('#sidebar').sidebar('attach events', '#sidebar-toggle', 'toggle');
+  // $('#sidebar').sidebar('setting', {
+  //   dimPage: false,
+  //   closable: false,
+  // });
 
-  $('.ui.checkbox').checkbox();
-  $('.ui.accordion').accordion();
-  $('.ui.menu .dropdown').dropdown({ action: 'hide' });
-  $('.ui.inline.dropdown').dropdown();
-  $('.link.ui.dropdown').dropdown({ action: 'hide' });
-  $('.button.ui.dropdown').dropdown({ action: 'hide' });
-  $('.ui.fluid.search.selection.ui.dropdown').dropdown();
-  $('.ui.tabular.menu .item, .owl-tabular-form .menu .item, .owl-tabular-show .item').tab();
-  $('.ui.card .dimmable.image, .ui.cards .card .dimmable.image').dimmer({ on: 'hover' });
-  $('.ui.rating').rating('disable');
+  // $('.ui.checkbox').checkbox();
+  // $('.ui.accordion').accordion();
+  // $('.ui.menu .dropdown').dropdown({ action: 'hide' });
+  // $('.ui.inline.dropdown').dropdown();
+  // $('.link.ui.dropdown').dropdown({ action: 'hide' });
+  // $('.button.ui.dropdown').dropdown({ action: 'hide' });
+  // $('.ui.fluid.search.selection.ui.dropdown').dropdown();
+  // $('.ui.tabular.menu .item, .owl-tabular-form .menu .item, .owl-tabular-show .item').tab();
+  // $('.ui.card .dimmable.image, .ui.cards .card .dimmable.image').dimmer({ on: 'hover' });
+  // $('.ui.rating').rating('disable');
 
-  $('form.loadable button[type=submit]').on('click', (event) => {
-    let $form = $(event.currentTarget).closest('form');
+  // $('form.loadable button[type=submit]').on('click', (event) => {
+  //   let $form = $(event.currentTarget).closest('form');
 
-    if(!$form.hasClass('is-ajax')) {
-      $(event.currentTarget).closest('form').addClass('loading');
-    }
-  });
-  $('.loadable.button').on('click', (event) => {
-    $(event.currentTarget).addClass('loading');
-  });
-  $('.message .close').on('click', (event) => {
-    $(event.currentTarget).closest('.message').transition('fade');
-  });
+  //   if(!$form.hasClass('is-ajax')) {
+  //     $(event.currentTarget).closest('form').addClass('loading');
+  //   }
+  // });
+  // $('.loadable.button').on('click', (event) => {
+  //   $(event.currentTarget).addClass('loading');
+  // });
+  // $('.message .close').on('click', (event) => {
+  //   $(event.currentTarget).closest('.message').transition('fade');
+  // });
 
-  $('[data-requires-confirmation]').requireConfirmation();
-  $('[data-bulk-action-requires-confirmation]').bulkActionRequireConfirmation();
-  $('[data-bulk-action-default]').bulkAction();
-  $('[data-toggles]').toggleElement();
-  $('[data-js-bulk-checkboxes]').checkAll();
+  // $('[data-requires-confirmation]').requireConfirmation();
+  // $('[data-bulk-action-requires-confirmation]').bulkActionRequireConfirmation();
+  // $('[data-bulk-action-default]').bulkAction();
+  // $('[data-toggles]').toggleElement();
+  // $('[data-js-bulk-checkboxes]').checkAll();
 
-  $('.special.cards .image').dimmer({
-    on: 'hover',
-  });
+  // $('.special.cards .image').dimmer({
+  //   on: 'hover',
+  // });
 
-  $('[data-form-type="collection"]').CollectionForm();
+  // $('[data-form-type="collection"]').CollectionForm();
 
-  $('[data-js-disable]').on('click', (e) => {
-    const $current = $(e.currentTarget);
-    $(document).find($current.attr('data-js-disable')).addClass('disabled');
-  });
+  // $('[data-js-disable]').on('click', (e) => {
+  //   const $current = $(e.currentTarget);
+  //   $(document).find($current.attr('data-js-disable')).addClass('disabled');
+  // });
 });
