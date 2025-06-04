91984b14fd86ee607a7aebc4c1bd88513b2f354c
Paweł Kęska <projekty@pawelkeska.eu>
2025-06-04 09:54:37 +0200

    [ECS] Fix all

--- src/Owl/Component/Core/Tests/Exception/HandleExceptionTest.php
+++ src/Owl/Component/Core/Tests/Exception/HandleExceptionTest.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace Tests\Owl\Component\Core\Exception;
 
-use PHPUnit\Framework\TestCase;
 use Owl\Component\Core\Exception\HandleException;
+use PHPUnit\Framework\TestCase;
 
 final class HandleExceptionTest extends TestCase
 {
